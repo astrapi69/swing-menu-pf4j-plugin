d9182a4616189531a3cc94c2b8b1b1e0544a5b04
astrapi69 <asterios.raptis@gmx.net>
2022-12-27 18:24:27 +0100

    formatted with spotless

--- src/test/java/io/github/astrapi69/DesktopMenuPluginTest.java
+++ src/test/java/io/github/astrapi69/DesktopMenuPluginTest.java
@@ -6,16 +6,8 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.*;
 
-import io.github.astrapi69.collection.list.ListExtensions;
-import io.github.astrapi69.file.create.FileFactory;
-import io.github.astrapi69.file.read.ReadFileExtensions;
-import io.github.astrapi69.file.search.PathFinder;
-import io.github.astrapi69.junit.jupiter.callback.before.test.IgnoreHeadlessExceptionExtension;
-import io.github.astrapi69.swing.action.ExitApplicationAction;
-import io.github.astrapi69.swing.action.NoAction;
-import io.github.astrapi69.swing.action.ToggleFullScreenAction;
-import io.github.astrapi69.swing.menu.enumeration.BaseMenuId;
-import io.github.astrapi69.throwable.RuntimeExceptionDecorator;
+import javax.swing.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.pf4j.DefaultExtensionFinder;
@@ -23,9 +15,17 @@ import org.pf4j.DefaultPluginManager;
 import org.pf4j.ExtensionFinder;
 import org.pf4j.PluginManager;
 
+import io.github.astrapi69.collection.list.ListExtensions;
+import io.github.astrapi69.file.create.FileFactory;
+import io.github.astrapi69.file.read.ReadFileExtensions;
+import io.github.astrapi69.file.search.PathFinder;
+import io.github.astrapi69.junit.jupiter.callback.before.test.IgnoreHeadlessExceptionExtension;
 import io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtensionPoint;
-
-import javax.swing.*;
+import io.github.astrapi69.swing.action.ExitApplicationAction;
+import io.github.astrapi69.swing.action.NoAction;
+import io.github.astrapi69.swing.action.ToggleFullScreenAction;
+import io.github.astrapi69.swing.menu.enumeration.BaseMenuId;
+import io.github.astrapi69.throwable.RuntimeExceptionDecorator;
 
 public class DesktopMenuPluginTest
 {
@@ -55,10 +55,13 @@ public class DesktopMenuPluginTest
 		// retrieve all extension points for "Greeting" extension point
 		List<DesktopMenuExtensionPoint> extensionPoints = pluginManager
 			.getExtensions(DesktopMenuExtensionPoint.class);
-		Optional<DesktopMenuExtensionPoint> desktopMenuExtensionPointOptional = ListExtensions.getFirst(extensionPoints);
+		Optional<DesktopMenuExtensionPoint> desktopMenuExtensionPointOptional = ListExtensions
+			.getFirst(extensionPoints);
 
-		if(desktopMenuExtensionPointOptional.isPresent()){
-			DesktopMenuExtensionPoint desktopMenuExtensionPoint = desktopMenuExtensionPointOptional.get();
+		if (desktopMenuExtensionPointOptional.isPresent())
+		{
+			DesktopMenuExtensionPoint desktopMenuExtensionPoint = desktopMenuExtensionPointOptional
+				.get();
 
 			Map<String, ActionListener> actionListenerMap;
 			String filename;
@@ -67,21 +70,26 @@ public class DesktopMenuPluginTest
 			actionListenerMap = new LinkedHashMap<>();
 			actionListenerMap.put(BaseMenuId.TOGGLE_FULLSCREEN.propertiesKey(),
 				new ToggleFullScreenAction("Fullscreen", new JFrame("Test Menu with xml")));
-			actionListenerMap.put(BaseMenuId.EXIT.propertiesKey(), new ExitApplicationAction("Exit"));
+			actionListenerMap.put(BaseMenuId.EXIT.propertiesKey(),
+				new ExitApplicationAction("Exit"));
 			actionListenerMap.put(BaseMenuId.FILE.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.MENU_BAR.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.HELP.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.HELP_CONTENT.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.HELP_DONATE.propertiesKey(), new NoAction());
 			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC.propertiesKey(), new NoAction());
-			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC_ACTIVITY.propertiesKey(), new NoAction());
-			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC_PROFILE.propertiesKey(), new NoAction());
+			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC_ACTIVITY.propertiesKey(),
+				new NoAction());
+			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC_PROFILE.propertiesKey(),
+				new NoAction());
 			actionListenerMap.put(TestMenuId.HELP_DIAGNOSTIC_USAGE.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.HELP_LICENSE.propertiesKey(), new NoAction());
 			actionListenerMap.put(BaseMenuId.HELP_INFO.propertiesKey(), new NoAction());
 
-			File xmlFile = FileFactory.newFileQuietly(PathFinder.getSrcTestResourcesDir(), filename);
-			String xml = RuntimeExceptionDecorator.decorate(() -> ReadFileExtensions.fromFile(xmlFile));
+			File xmlFile = FileFactory.newFileQuietly(PathFinder.getSrcTestResourcesDir(),
+				filename);
+			String xml = RuntimeExceptionDecorator
+				.decorate(() -> ReadFileExtensions.fromFile(xmlFile));
 			JMenuBar jMenuBar = desktopMenuExtensionPoint.buildMenuBar(xml, actionListenerMap);
 			assertNotNull(jMenuBar);
 		}
