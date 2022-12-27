1842c22610b7f8a15d4617c3eba760b88bc8b2b6
astrapi69 <asterios.raptis@gmx.net>
2022-12-27 17:23:15 +0100

    formatted with spotless

--- src/test/java/io/github/astrapi69/DesktopMenuPluginTest.java
+++ src/test/java/io/github/astrapi69/DesktopMenuPluginTest.java
@@ -1,18 +1,19 @@
 package io.github.astrapi69;
 
-import io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtensionPoint;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.pf4j.DefaultExtensionFinder;
 import org.pf4j.DefaultPluginManager;
 import org.pf4j.ExtensionFinder;
 import org.pf4j.PluginManager;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtensionPoint;
 
 public class DesktopMenuPluginTest
 {
@@ -21,11 +22,14 @@ public class DesktopMenuPluginTest
 	public void testPlugin()
 	{
 		// create the plugin manager
-		final PluginManager pluginManager = new DefaultPluginManager() {
+		final PluginManager pluginManager = new DefaultPluginManager()
+		{
 
-			protected ExtensionFinder createExtensionFinder() {
-				DefaultExtensionFinder extensionFinder = (DefaultExtensionFinder) super.createExtensionFinder();
-				extensionFinder.addServiceProviderExtensionFinder(); // to activate "HowdyGreeting" extension
+			protected ExtensionFinder createExtensionFinder()
+			{
+				DefaultExtensionFinder extensionFinder = (DefaultExtensionFinder)super.createExtensionFinder();
+				extensionFinder.addServiceProviderExtensionFinder(); // to activate "HowdyGreeting"
+																		// extension
 
 				return extensionFinder;
 			}
@@ -36,13 +40,16 @@ public class DesktopMenuPluginTest
 		pluginManager.startPlugins();
 
 		// retrieve all extension points for "Greeting" extension point
-		List<DesktopMenuExtensionPoint> extensionPoints = pluginManager.getExtensions(DesktopMenuExtensionPoint.class);
-		for (DesktopMenuExtensionPoint extensionPoint : extensionPoints) {
+		List<DesktopMenuExtensionPoint> extensionPoints = pluginManager
+			.getExtensions(DesktopMenuExtensionPoint.class);
+		for (DesktopMenuExtensionPoint extensionPoint : extensionPoints)
+		{
 			assertNotNull(extensionPoint);
 		}
 		Set<String> extensionClassNames = pluginManager.getExtensionClassNames(null);
 		assertTrue(extensionClassNames.contains("org.pf4j.processor.ExtensionAnnotationProcessor"));
-		assertTrue(extensionClassNames.contains("io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtension"));
+		assertTrue(extensionClassNames
+			.contains("io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtension"));
 
 		// stop and unload all plugins
 		pluginManager.stopPlugins();
