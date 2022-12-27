318531e5c89f20e7513afd48b11268ed51e9703d
astrapi69 <asterios.raptis@gmx.net>
2022-12-27 17:13:28 +0100

    added new dependency menu-action in version 2.6
    - added new dependency gen-tree in version 7.4
    - adapted extension point for create JMenuBar with xml and an action map

--- src/main/java/io/github/astrapi69/menu/pf4j/extension/DesktopMenuExtension.java
+++ src/main/java/io/github/astrapi69/menu/pf4j/extension/DesktopMenuExtension.java
@@ -1,17 +1,47 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2022 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package io.github.astrapi69.menu.pf4j.extension;
 
-import javax.swing.JMenu;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+
 import org.pf4j.Extension;
 
-@Extension
-public class DesktopMenuExtension implements DesktopMenuExtensionPoint {
+import io.github.astrapi69.swing.menu.factory.JMenuBarFactory;
+import io.github.astrapi69.swing.menu.model.transform.MenuInfoTreeNodeConverter;
 
-    public void buildMenuBar(JMenuBar menuBar) {
-        JMenu exampleMenu = new JMenu("Example");
-        exampleMenu.add(new JMenuItem("Hello World"));
-        menuBar.add(exampleMenu);
-    }
+@Extension
+public class DesktopMenuExtension implements DesktopMenuExtensionPoint
+{
+
+	public JMenuBar buildMenuBar(String xml, Map<String, ActionListener> actionListenerMap)
+	{
+		return JMenuBarFactory.buildMenuBar(MenuInfoTreeNodeConverter.toMenuInfoTreeNode(xml),
+			actionListenerMap);
+	}
 
 }
--- src/main/java/io/github/astrapi69/menu/pf4j/plugin/DesktopMenuPlugin.java
+++ src/main/java/io/github/astrapi69/menu/pf4j/plugin/DesktopMenuPlugin.java
@@ -1,6 +1,32 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2022 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package io.github.astrapi69.menu.pf4j.plugin;
 
-import io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtensionPoint;
+import java.util.List;
+import java.util.Set;
+
 import org.pf4j.DefaultExtensionFinder;
 import org.pf4j.DefaultPluginManager;
 import org.pf4j.ExtensionFinder;
@@ -8,40 +34,47 @@ import org.pf4j.Plugin;
 import org.pf4j.PluginManager;
 import org.pf4j.PluginWrapper;
 
-import java.util.List;
-import java.util.Set;
+import io.github.astrapi69.menu.pf4j.extension.DesktopMenuExtensionPoint;
 
 public class DesktopMenuPlugin extends Plugin
 {
 	public DesktopMenuPlugin(PluginWrapper wrapper)
 	{
 		super(wrapper);
-		// you can use "wrapper" to have access to the plugin context (plugin manager, descriptor, ...)
+		// you can use "wrapper" to have access to the plugin context (plugin manager, descriptor,
+		// ...)
 	}
 
-	@Override public void start()
+	@Override
+	public void start()
 	{
 		System.err.println("DesktopMenuPlugin.start()");
 	}
 
-	@Override public void stop()
+	@Override
+	public void stop()
 	{
 		System.err.println("DesktopMenuPlugin.stop()");
 	}
 
-	@Override public void delete()
+	@Override
+	public void delete()
 	{
 		System.err.println("DesktopMenuPlugin.delete()");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// create the plugin manager
 		// create the plugin manager
-		final PluginManager pluginManager = new DefaultPluginManager() {
+		final PluginManager pluginManager = new DefaultPluginManager()
+		{
 
-			protected ExtensionFinder createExtensionFinder() {
-				DefaultExtensionFinder extensionFinder = (DefaultExtensionFinder) super.createExtensionFinder();
-				extensionFinder.addServiceProviderExtensionFinder(); // to activate "HowdyGreeting" extension
+			protected ExtensionFinder createExtensionFinder()
+			{
+				DefaultExtensionFinder extensionFinder = (DefaultExtensionFinder)super.createExtensionFinder();
+				extensionFinder.addServiceProviderExtensionFinder(); // to activate "HowdyGreeting"
+																		// extension
 
 				return extensionFinder;
 			}
@@ -52,12 +85,15 @@ public class DesktopMenuPlugin extends Plugin
 		pluginManager.startPlugins();
 
 		// retrieve all extensions for "Greeting" extension point
-		List<DesktopMenuExtensionPoint> extensions = pluginManager.getExtensions(DesktopMenuExtensionPoint.class);
-		for (DesktopMenuExtensionPoint menuExtensionPoint : extensions) {
+		List<DesktopMenuExtensionPoint> extensions = pluginManager
+			.getExtensions(DesktopMenuExtensionPoint.class);
+		for (DesktopMenuExtensionPoint menuExtensionPoint : extensions)
+		{
 			System.out.println(">>> " + menuExtensionPoint);
 		}
 		Set<String> extensionClassNames = pluginManager.getExtensionClassNames(null);
-		for (String extension : extensionClassNames) {
+		for (String extension : extensionClassNames)
+		{
 			System.out.println("   " + extension);
 		}
 		// stop and unload all plugins
